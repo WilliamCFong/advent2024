c325aefbfeca98c01af971c0c860f67a7dfb81e1
William Fong <willfong@mit.edu>
2024-12-04 06:47:43 -0700

    updating tests for parallel processing

--- noxfile.py
+++ noxfile.py
@@ -1,9 +1,11 @@
 import nox
 
+
 @nox.session
 def tests(session):
     session.install("pytest", ".")
-    session.run("pytest")
+    session.run("pytest", "-n", "auto")
+
 
 @nox.session
 def lint(session):
